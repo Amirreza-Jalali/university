3b70e65eb5b1a6e45cd159e72251f5124e95376c
Amirreza-Jalali <96478082+Amirreza-Jalali@users.noreply.github.com>
2025-12-10 09:20:57 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  merkel tree.py
+//
+//  Created by Apple on 6/26/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
